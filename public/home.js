4307559b566f2516113002bc82c01730212ca5cd
Piet RAGOLANE <pragolan@e5r11p7.wethinkcode.co.za>
2019-01-11 16:58:59 +0200

    After

--- public/home.js
+++ public/home.js
@@ -1,3 +1,50 @@
+var player;
+
+function  onYouTubeIframeAPIReady(){
+player = new YT.Player('player', {
+    height: '380',
+    width: '900',
+    videoId: 'tqErAlg-QJU',
+    events: {
+      'onReady': function (e) {
+        e.target.playVideo();
+        // pause video after 3 seconds
+        setTimeout(function() {
+          player.pauseVideo();
+        }, 3 * 1000);
+      },
+      'onStateChange': function (e) {
+        // if video ended (0) or paused (2)....
+        if (e.data == 0 || e.data == 2) {
+          // remove the player
+          player.destroy();
+          // toggle the visibility
+          $('#icanvas, #player').toggle();
+        }
+      }
+    }
+  });
+}
+
+//$(function(){
+  //  $('#icanvas').hide();
+//});
+/*document.addEventListener('DOMContentLoaded', function(){
+    var video = document.getElementById('vid');
+    var canvas = document.getElementById('icanvas');
+    var context = canvas.getContext('2d');
+
+    video.addEventListener('play', function(){
+        draw(this, context, canvas.width, canvas.height);
+    }, false);
+}, false);
+
+function draw(v, c, w, h){
+    if(v.paused || v.ended) return false;
+    c.draw(v, 0, 0, w, h);
+    setTimeout(draw, 20, v, c, w, h);
+}*/
+
 function signUp(){
     var login = document.getElementById("login");
     var register  = document.getElementById("register");
