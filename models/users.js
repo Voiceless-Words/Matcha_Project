01ef5696ad217b8286a177d0acce2fd88fed99ec
Piet RAGOLANE <pragolan@e4r5p5.wethinkcode.co.za>
2019-01-24 14:21:24 +0200

    Done for the day

--- models/users.js
+++ models/users.js
@@ -1,29 +1,39 @@
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var SALT_WORK_FACTOR = 10;
+var Schema = mongoose.Schema;
 
-//usrs schema
-let userSchema = mongoose.Schema({
-    first_name:{
-        type: String,
-        required: true
-    },
-    last_name:{
-        type: String,
-        required: true
-    },
-    username:{
-        type: String,
-        required: true
-    },
-    email:{
-        type: String,
-        required: true
-    },
-    password:{
-        type: String,
-        required: true
-    },
-},
-    { versionKey: false
+var userSchema = new Schema({
+    username: {type:String, required: true, index:{unique:true}},
+    firstname: {type: String, required: true},
+    lastname: {type: String, required: true},
+    email: {type:String, required: true},
+    password: {type: String, required: true}
 });
 
-let Users = module.exports = mongoose.model('Users', userSchema);
+userSchema.pre('save', function(next){
+    var user = this;
+
+    if(!user.isModified('password')) return next();
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, function(err, hash){
+            if (err) return next(err);
+
+            user.password = hash;
+            next();
+        });
+    });
+
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb){
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+module.exports = mongoose.model('Users', 'userSchema');
